7a93229528c0701fcf1025e74e1abaaece7be415
Đặng Tuấn Ninh <109080992+DangTuanNinh@users.noreply.github.com>
2024-09-13 10:49:55 +0700

    Implemented task 1

---
+++ script.js
@@ -0,0 +1,45 @@
+// Age-ify (A future age calculator)
+
+// const yearOfBirth = 2003;
+// const yearFuture = 2024;
+// const age = yearFuture - yearOfBirth;
+// console.log(`You will be ${age} years old in ${yearFuture}`);
+
+// Goodboy-Oldboy (A dog age calculator)
+
+// const dogYearOfBirth = 2030;
+// const dogYearFuture = 2045;
+// const dogYear = dogYearFuture - dogYearOfBirth;
+
+// const shouldShowResultInDogYears = true;
+// if (shouldShowResultInDogYears)
+//   console.log(
+//     `Your dog will be ${dogYear * 7} dog years old in ${dogYearFuture}`
+//   );
+// else
+//   console.log(
+//     `Your dog will be ${dogYear} human years old in ${dogYearFuture}`
+//   );
+
+// Housey pricey (A house price estimator)
+
+// const width = 5;
+// const height = 11;
+// const deep = 8;
+// const volumeInMeters = width * height * deep;
+// const gardenSizeInM2 = 70;
+// const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+// console.log(housePrice);
+
+// Ez Namey (Startup name generator) Optional
+
+// const randomNumber = Math.floor(Math.random() * 3);
+// const randomNumber1 = Math.floor(Math.random() * 3);
+// const firstWords = ["Easy", "Awesome", "Corporate"];
+// const secondWords = ["Easy", "Awesome", "Corporate"];
+// const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber1];
+// const nameLength = startupName.length;
+
+// console.log(`The startup: ${startupName} contains ${nameLength} characters`);
+
+
